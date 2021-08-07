972323005577143382e513564728416f9f84d36f
clod <claudiu.codreanu@gmail.com>
2021-08-07 14:53:46 -0400

    Removes comments from the top of the file

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-
-/*
-    Finds and displays the moves which solve the Tower of Hanoi puzzle.
-
-      n:       number of disks
-      from:    the initial peg, where disks are stacked on top of each other
-      to:      the destination peg, where disks must be moved
-
-    Restrictions:
-    
-      - disks must be moved one at a time
-      - you can pick a disk from the top of any pile, and place it on top of any other pile
-      - but you cannot place a disk on top of a smaller disk
-      - you must place it on top of a larger disk
-      - so at any time, all piles are sorted pyramid-like: from larger at bottom, to smaller at top
-
-    Algorithm:
-
-    Once you understand the main idea, the implementation is quite simple.
-    
-      1. To move a stack of N disks from source peg to destination peg...
-      2. You need to move the top N - 1 disks to the other, helper peg...
-      3. Which releases the largest Nth disk at the bottom, so you can move it to destination peg...
-      4. Then you move the N - 1 pile from helper peg, to the destination peg
-
-    Steps 2 and 4 are more of the same: you need to move a stack of M = N - 1 disks.
-    So the recursion is quite evident.
-
-    The recursion stops for N == 1, when there's no need to drill any further.
-
-    Also, the puzzle can be solved in a minimum number of moves, which is N-dependent:
-    Min number of moves = 2 ** N - 1 (two to the power of N, minus 1)
-*/
-
 function hanoi(n, from, to) {
     from--;
     to--;
